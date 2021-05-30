ea0d6aeac9be04ce540c53e16f4e4d0b189d5579
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-05-30 20:16:22 -0300

    clean up code

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -1,8 +1,6 @@
 package academy.learnprogramming.console;
 
 import academy.learnprogramming.config.AppConfig;
-import academy.learnprogramming.MessageGenerator;
-import academy.learnprogramming.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,9 +15,6 @@ public class Main {
 
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
-
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
         context.close();
     }
 }
