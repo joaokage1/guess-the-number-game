0db943d37cd1221126170edbdd52cd4043db0702
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-05-30 19:29:40 -0300

    using properties

--- core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -2,15 +2,20 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.GuessCount;
 import academy.learnprogramming.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
-    private int maxNumber = 50;
+    @Value("${max.number}")
+    private int maxNumber;
 
-    private int guessCount = 8;
+    @Value("${guess.count}")
+    private int guessCount;
 
     @Bean
     @MaxNumber
